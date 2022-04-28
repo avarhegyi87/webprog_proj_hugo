1f8f3ccd7c384257028d2c812f3c7351fb297752
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-04-28 18:09:42 +0200

    adjsuted the "your message" div

--- templates/pages/yourmsg.tpl.php
+++ templates/pages/yourmsg.tpl.php
@@ -4,48 +4,50 @@ if (
     isset($_POST['gender']) && isset($_POST['email']) && isset($_POST['question'])
 ) {
 ?>
-    <div id="msg-display">
-        <table>
-            <tbody>
-                <tr>
-                    <th>Your name:</th>
-                    <td><?= $_POST['fname'] . ' ' . $_POST['lname'] ?></td>
-                </tr>
-                <tr>
-                    <th>Your gender:</th>
-                    <td>
-                        <?php
-                        switch ($_POST['gender']) {
-                            case 'male':
-                                echo 'Male';
-                                break;
-                            case 'female':
-                                echo 'Female';
-                                break;
-                            case 'other':
-                                echo 'Other / Rather not disclose';
-                                break;
-                            default:
-                                echo 'not provided';
-                                break;
-                        }
-                        ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Your email address:</th>
-                    <td><?= $_POST['email'] ?></td>
-                </tr>
-                <tr>
-                    <th>Signed up to newsletter?</th>
-                    <td><?= (isset($_POST['newsletter'])) ? 'Yes' : 'No'; ?></td>
-                </tr>
-                <tr>
-                    <th>Your message to us</th>
-                    <td><?= $_POST['question'] ?></td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="col-md-6 mx-auto">
+        <div id="msg-display">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Your name:</th>
+                        <td><?= $_POST['fname'] . ' ' . $_POST['lname'] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Your gender:</th>
+                        <td>
+                            <?php
+                            switch ($_POST['gender']) {
+                                case 'male':
+                                    echo 'Male';
+                                    break;
+                                case 'female':
+                                    echo 'Female';
+                                    break;
+                                case 'other':
+                                    echo 'Other / Rather not disclose';
+                                    break;
+                                default:
+                                    echo 'not provided';
+                                    break;
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Your email address:</th>
+                        <td><?= $_POST['email'] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Signed up to newsletter?</th>
+                        <td><?= (isset($_POST['newsletter'])) ? 'Yes' : 'No'; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Your message to us</th>
+                        <td><?= $_POST['question'] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 <?php
 } else { ?>
